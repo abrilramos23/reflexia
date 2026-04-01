d9e9ff748ae457268655f7715d344ad433f8b792
abrilRamosDom <abrilramos2004@gmail.com>
2026-04-01 22:28:13 +0200

    fix: change frontend eslint test configuration to avoid irrelevant errors

--- reflexia-frontend/eslint.config.js
+++ reflexia-frontend/eslint.config.js
@@ -5,12 +5,18 @@ export default [
   {
     files: ["**/*.{js,mjs,cjs,jsx}"],
     languageOptions: { globals: globals.browser },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
   },
   {
     ...pluginReact.configs.flat.recommended,
     rules: {
       ...pluginReact.configs.flat.recommended.rules,
       "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
     },
   },
 ];
